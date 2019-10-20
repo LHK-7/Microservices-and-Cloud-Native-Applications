5a1314c785ea07c002306dbb3b163de4f28494a0
maruibin <maruibin123@gmail.com>
2019-10-20 15:41:24 -0400

    Update email_verification.py

--- email_verification.py
+++ email_verification.py
@@ -1,22 +1,23 @@
 import json
-import jwt as apij
+import jwt
 
 import boto3
 import logging
 from botocore.exceptions import ClientError
 
-# Replace sender@example.com with your "From" address.
 # This address must be verified with Amazon SES.
 #SENDER = "Donald F. Ferguson <dff@cs.columbia.edu>"
 SENDER = "Info <ryliegao@gmail.com>"
+LINK = "https://7860me252b.execute-api.us-east-1.amazonaws.com/s1"
 
 # Specify a configuration set. If you do not want to use a configuration
 # set, comment the following variable, and the 
 # ConfigurationSetName=CONFIGURATION_SET argument below.
 CONFIGURATION_SET = "ConfigSet"
 
-# If necessary, replace us-west-2 with the AWS Region you're using for Amazon SES.
 AWS_REGION = "us-east-1"
+ACCESS_KEY = '' # DO NOT EXPOSE keys here publicly.
+SECRET_KEY = ''
 
 # The subject line for the email.
 SUBJECT = "Cool message from Don!!!"
@@ -47,13 +48,12 @@ BODY_HTML = """<html>
 # The character encoding for the email.
 CHARSET = "UTF-8"
 
-LINK = "https://7860me252b.execute-api.us-east-1.amazonaws.com/s1"
 
 # Create a new SES resource and specify a region.
 client = boto3.client('ses',
     region_name=AWS_REGION,
-    aws_access_key_id='AKIAJKMANSAMMSGXDOWA',
-    aws_secret_access_key='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'
+    aws_access_key_id=ACCESS_KEY,
+    aws_secret_access_key=SECRET_KEY
 )
 
 _secret = "secret"
@@ -66,9 +66,9 @@ _secret = "secret"
 # Try to send the email.
 def send_email(em):
     try:
-        logger.info("em = ", em)
+        logger.info("em = " + em)
 
-        tok = apij.encode({'email': em}, key=_secret)
+        tok = jwt.encode({'email': em}, key=_secret)
         logger.info("Encoded = " + str(tok))
 
         #Provide the contents of the email.
@@ -94,7 +94,7 @@ def send_email(em):
                     'Data': SUBJECT,
                 },
             },
-               Source=SENDER
+            Source=SENDER
             # If you are not using a configuration set, comment or delete the
             # following line
             #ConfigurationSetName=CONFIGURATION_SET,
@@ -109,13 +109,21 @@ def send_email(em):
 def handle_sns_event(records):
 
     sns_event = records[0]['Sns']
-    # topic_arn = sns_event.get("TopicArn", None)
-    # topic_subject = sns_event.get("Subject", None)
+    topic_arn = sns_event.get("TopicArn", None)
+    topic_subject = sns_event.get("Subject", None)
     topic_msg = sns_event.get("Message", None)
 
-    # logger.info("SNS Subject = ", topic_subject)
-    em = topic_msg["customers_email"]
-    send_email(em)
+    print("SNS Subject = ", topic_subject)
+    if topic_msg:
+        json_msg = None
+        try:
+            json_msg = json.loads(json.loads(topic_msg))
+            print("Message = ", json.dumps(json_msg, indent=2))
+        except:
+            print("Could not parse message.")
+
+        em = json_msg["customers_email"]
+        send_email(em)
 
 
 def lambda_handler(event, context):
