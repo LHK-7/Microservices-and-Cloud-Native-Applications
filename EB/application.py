901a644ecaa50a2d6e4019b7285dfc2494d315a5
maruibin <maruibin123@gmail.com>
2019-10-22 16:07:45 -0400

    Tested on Elastic Beanstalk
    Needs to set Env Vars:
    test = {"host":"database-6156.cbl6qjbnc3gz.us-east-1.rds.amazonaws.com","user": "admin","password": "woshishabi","db": "innodb","charset":"utf8mb4","port":3306}

--- EB/application.py
+++ EB/application.py
@@ -1,25 +1,32 @@
+# ENV VARS: test={"host":"localhost","user":"root","password":"123","port":3306,"db":"e6156","charset":"utf8mb4"}
+# {"host":"database-6156.cbl6qjbnc3gz.us-east-1.rds.amazonaws.com","user": "admin","password": "woshishabi","db": "innodb","charset":"utf8mb4","port":3306}
+# Table Name MUST be "users"
 
 # Import functions and objects the microservice needs.
 # - Flask is the top-level application. You implement the application by adding methods to it.
 # - Response enables creating well-formed HTTP/REST responses.
 # - requests enables accessing the elements of an incoming HTTP/REST request.
-#
+
+import uuid
+
+from flask import Flask, Response, request, render_template
+from werkzeug.utils import redirect
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from datetime import datetime
 import json
+
+from CustomerInfo.Users import UsersService as UserService
+from Context.Context import Context
+
 # Setup and use the simple, common Python logging framework. Send log messages to the console.
 # The application should get the log level out of the context. We will change later.
 #
 import logging
-import uuid
-from datetime import datetime
-
-from Context.Context import Context
-from CustomerInfo.Users import UsersService as UserService
-from flask import Flask, Response, request, render_template
-from wtforms import Form, StringField, PasswordField, validators
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
+
 
 ###################################################################################################################
 #
@@ -27,8 +34,9 @@ logger.setLevel(logging.DEBUG)
 #
 # AWS puts this function in the default started application
 # print a nice greeting.
-def say_hello(username = "World"):
+def say_hello(username="World"):
     return '<p>Hello %s!</p>\n' % username
+
 
 # AWS put this here.
 # some bits of text for the page.
@@ -47,12 +55,12 @@ application = Flask(__name__)
 
 # add a rule for the index page. (Put here by AWS in the sample)
 application.add_url_rule('/', 'index', (lambda: header_text +
-    say_hello() + instructions + footer_text))
+                                                say_hello() + instructions + footer_text))
 
 # add a rule when the page is accessed with a name appended to the site
 # URL. Put here by AWS in the sample
 application.add_url_rule('/<username>', 'hello', (lambda username:
-    header_text + say_hello(username) + home_link + footer_text))
+                                                  header_text + say_hello(username) + home_link + footer_text))
 
 ##################################################################################################################
 # The stuff I added begins here.
@@ -62,7 +70,6 @@ _user_service = None
 
 
 def _get_default_context():
-
     global _default_context
 
     if _default_context is None:
@@ -81,7 +88,6 @@ def _get_user_service():
 
 
 def init():
-
     global _default_context, _user_service
 
     _default_context = Context.get_default_context()
@@ -95,7 +101,6 @@ def init():
 # 3. Return extracted information.
 #
 def log_and_extract_input(method, path_params=None):
-
     path = request.path
     args = dict(request.args)
     data = None
@@ -113,14 +118,14 @@ def log_and_extract_input(method, path_params=None):
 
     log_message = str(datetime.now()) + ": Method " + method
 
-    inputs =  {
+    inputs = {
         "path": path,
         "method": method,
         "path_params": path_params,
         "query_params": args,
         "headers": headers,
         "body": data
-        }
+    }
 
     log_message += " received: \n" + json.dumps(inputs, indent=2)
     logger.debug(log_message)
@@ -129,7 +134,6 @@ def log_and_extract_input(method, path_params=None):
 
 
 def log_response(method, status, data, txt):
-
     msg = {
         "method": method,
         "status": status,
@@ -143,40 +147,45 @@ def log_response(method, status, data, txt):
 # This function performs a basic health check. We will flesh this out.
 @application.route("/health", methods=["GET"])
 def health_check():
-
-    rsp_data = { "status": "healthy", "time": str(datetime.now()) }
+    rsp_data = {"status": "healthy", "time": str(datetime.now())}
     rsp_str = json.dumps(rsp_data)
     rsp = Response(rsp_str, status=200, content_type="application/json")
     return rsp
 
 
+# Demo. Return the received inputs.
 @application.route("/demo/<parameter>", methods=["GET", "POST"])
 def demo(parameter):
-
-    inputs = log_and_extract_input(demo, { "parameter": parameter })
+    inputs = log_and_extract_input(demo, {"parameter": parameter})
 
     msg = {
-        "/demo received the following inputs" : inputs
+        "/demo received the following inputs": inputs
     }
 
     rsp = Response(json.dumps(msg), status=200, content_type="application/json")
     return rsp
 
 
-class registerForm(Form):
+class RegisterForm(Form):
     last_name = StringField('Last Name', [validators.Length(min=1, max=50)])
     first_name = StringField('First Name', [validators.Length(min=1, max=50)])
-    email = StringField('Email', [validators.length(min=6,max=50)])
-    password = PasswordField('Password',[
+    email = StringField('Email', [validators.length(min=6, max=50)])
+    password = PasswordField('Password', [
         validators.DataRequired()
     ])
 
+# REDIRECT
+@application.route("/api/redirect", methods=["GET"])
+def redir():
+    return redirect("http://www.example.com", code=302)
 
-@application.route("/api/user/registeration",  methods=["GET","POST"])
+
+# REGISTER
+@application.route("/api/user/registeration", methods=["GET", "POST"])
 def register_user():
     global _user_service
 
-    form = registerForm(request.form)
+    form = RegisterForm(request.form)
     if request.method == 'POST' and form.validate():
         last_name = form.last_name.data
         first_name = form.first_name.data
@@ -184,8 +193,8 @@ def register_user():
         password = form.password.data
         id = str(uuid.uuid4())
 
-        res = [id, last_name,first_name,email,password]
-        temp ={"id": res[0], "last_name": res[1], "first_name":res[2], "email": res[3], "password": res[4]}
+        res = [id, last_name, first_name, email, password]
+        temp = {'id': res[0], 'last_name': res[1], 'first_name': res[2], 'email': res[3], 'password': res[4]}
 
         print(res)
         print(temp)
@@ -194,13 +203,12 @@ def register_user():
         rsp = user_service.create_user(temp)
         return render_template('register.html', form=form)
 
-        #return render_template('register.html', form=form)
+        # return render_template('register.html', form=form)
     return render_template('register.html', form=form)
 
-
+# QUERY user info by email
 @application.route("/api/user/<email>", methods=["GET", "PUT", "DELETE"])
 def user_email(email):
-
     global _user_service
 
     inputs = log_and_extract_input(demo, {"parameters": email})
@@ -227,22 +235,12 @@ def user_email(email):
                 rsp_txt = "NOT FOUND"
 
         elif request.method == 'PUT':
-            form = registerForm(request.form)
+            temp = {"email": email, "status": "ACTIVE"}
+            rsp_data = user_service.update_user(temp)
+            rsp_status = 200
+            rsp_txt = str(rsp_data)
 
-            if form.validate():
-                last_name = form.last_name.data
-                first_name = form.first_name.data
-                email = form.email.data
-                password = form.password.data
-                id = str(uuid.uuid4())
-
-                res = [id, last_name, first_name, email, password]
-                temp = {"id": res[0], "last_name": res[1], "first_name": res[2], "email": res[3], "password": res[4]}
-
-                user_service = _get_user_service()
-                rsp = user_service.update_user(temp)
-
-        elif inputs["method"] == "DELETE":
+        elif inputs["method"] == "DELETE": # This SHOULD SET STATUS to DELETED instead of removing the tuple
             rsp_data = user_service.delete_user({"email": email})
             rsp_status = 200
             rsp_txt = str(rsp_data)
