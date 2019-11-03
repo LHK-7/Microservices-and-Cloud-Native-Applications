7ac3b9f8010b36b88ab41beb4715a8377f578ee0
LHK-7 <55151029+LHK-7@users.noreply.github.com>
2019-11-02 21:00:31 -0400

    Update DataObject.py

--- EB/DataAccess/DataObject.py
+++ EB/DataAccess/DataObject.py
@@ -1,6 +1,8 @@
-import DataAccess.DataAdaptor as data_adaptor
-from abc import ABC, abstractmethod
+import EB.DataAccess.DataAdaptor as data_adaptor
 import pymysql.err
+
+from abc import ABC, abstractmethod
+
 
 class DataException(Exception):
 
@@ -10,6 +12,7 @@ class DataException(Exception):
     def __init__(self, code=unknown_error, msg="Something awful happened."):
         self.code = code
         self.msg = msg
+
 
 class BaseDataObject(ABC):
 
@@ -32,10 +35,10 @@ class UsersRDB(BaseDataObject):
     @classmethod
     def get_by_email(cls, email):
 
-        sql = "select * from e6156.users where email=%s"
+        sql = "select * from users where email=%s"
         res, data = data_adaptor.run_q(sql=sql, args=(email), fetch=True)
         if data is not None and len(data) > 0:
-            result =  data[0]
+            result = data[0]
         else:
             result = None
 
@@ -63,8 +66,44 @@ class UsersRDB(BaseDataObject):
 
         return result
 
+    @classmethod
+    def delete_user(cls, user_info):
+        if not user_info or not user_info["email"]:
+            raise ValueError("Error: User must be deleted by a given email.")
+        try:
+            sql, args = data_adaptor.create_delete(table_name="users", template=user_info)
+            res, data = data_adaptor.run_q(sql, args)
+            result = res
+        except Exception as exp:
+            raise exp
 
+        return result
 
+    @classmethod
+    def update_user(cls, user_info,template):
+        result = None
+        try:
+            sql, args = data_adaptor.create_update(table_name="users", new_values=user_info,template=template)
+            res, data = data_adaptor.run_q(sql, args)
+            if res != 1:
+                result = None
+            else:
+                result = user_info['id']
+        except Exception as e:
+            raise DataException()
 
+        return result
 
+    @classmethod
+    def validate_info(cls,user_info):
+        try:
+            sql = "select password from users where email = "+ "'" + user_info + "'"
+            res, data = data_adaptor.run_q(sql)
+            if res != 1:
+                result = None
+            else:
+                res = data[0].get("password")
+        except Exception as e:
+            raise DataException()
 
+        return res
