d7fbe1321cfcba9528675c441454243917f49fbe
maruibin <maruibin123@gmail.com>
2019-12-14 11:11:28 -0500

    fix email verification lambda

--- lambda/email_verification.py
+++ lambda/email_verification.py
@@ -1,6 +1,7 @@
 import json
 import jwt
 import requests
+# from requests import Request, Session
 import boto3
 import logging
 from botocore.exceptions import ClientError
@@ -10,16 +11,15 @@ from botocore.exceptions import ClientError
 SENDER = "Info <ryliegao@gmail.com>"
 
 # API Gateway
-LINK = "https://ebvcfzzsg1.execute-api.us-east-1.amazonaws.com/test/verifyemail" 
+LINK = "https://ebvcfzzsg1.execute-api.us-east-1.amazonaws.com/test/verifyemail"
 
 # Validation RESTful server (update user status)
-ENDPOINT = "http://e6156yeah.us-east-2.elasticbeanstalk.com/" 
+ENDPOINT = "http://e6156yeah.us-east-2.elasticbeanstalk.com/"
 
 # Specify a configuration set. If you do not want to use a configuration
-# set, comment the following variable, and the 
+# set, comment the following variable, and the
 # ConfigurationSetName=CONFIGURATION_SET argument below.
 CONFIGURATION_SET = "ConfigSet"
-
 
 # The subject line for the email.
 SUBJECT = "Cool message from Don!!!"
@@ -28,7 +28,7 @@ SUBJECT = "Cool message from Don!!!"
 BODY_TEXT = ("Amazon SES Test (Python)\r\n"
              "This email was sent with Amazon SES using the "
              "AWS SDK for Python (Boto)."
-            )
+             )
 
 # The HTML body of the email.
 BODY_HTML = """<html>
@@ -45,18 +45,17 @@ BODY_HTML = """<html>
     </form>
 </body>
 </html>
-            """            
+            """
 
 # The character encoding for the email.
 CHARSET = "UTF-8"
 
-
 # Create a new SES resource and specify a region.
 client = boto3.client('ses',
-    # region_name=AWS_REGION,
-    # aws_access_key_id=ACCESS_KEY,
-    # aws_secret_access_key=SECRET_KEY
-)
+                      # region_name=AWS_REGION,
+                      # aws_access_key_id=ACCESS_KEY,
+                      # aws_secret_access_key=SECRET_KEY
+                      )
 
 # The secret key for jwt encoding.
 _secret = "secret"
@@ -66,6 +65,7 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger()
 logger.setLevel(level=logging.DEBUG)
 
+
 # Try to send the email.
 def send_email(em):
     try:
@@ -74,7 +74,7 @@ def send_email(em):
         tok = jwt.encode({'email': em}, key=_secret).decode()
         logger.info("Encoded = " + str(tok))
 
-        #Provide the contents of the email.
+        # Provide the contents of the email.
         response = client.send_email(
             Destination={
                 'ToAddresses': [
@@ -100,19 +100,19 @@ def send_email(em):
             Source=SENDER
             # If you are not using a configuration set, comment or delete the
             # following line
-            #ConfigurationSetName=CONFIGURATION_SET,
-            )
-            
-    # Display an error if something goes wrong. 
+            # ConfigurationSetName=CONFIGURATION_SET,
+        )
+
+    # Display an error if something goes wrong.
     except ClientError as e:
         logger.info(e.response['Error']['Message'])
-        
+
     else:
         logger.info("Email sent! Message ID:"),
         logger.info(response['MessageId'])
 
-def handle_sns_event(records):
 
+def handle_sns_event(records):
     sns_event = records[0]['Sns']
     topic_arn = sns_event.get("TopicArn", None)
     topic_subject = sns_event.get("Subject", None)
@@ -130,47 +130,49 @@ def handle_sns_event(records):
         em = json_msg["customers_email"]
         send_email(em)
 
+
 def handle_api_event(token):
     try:
-        decoded = jwt.decode(token.encode(), key=_secret) # type: dict
-        email = decoded["email"] # type: str
-    
+        decoded = jwt.decode(token.encode(), key=_secret)  # type: dict
+        email = decoded["email"]  # type: str
+
         # Call RESTful microservice. Validate the token and Update user status PENDING -> ACTIVE.
         URL = str(ENDPOINT + "/api/user/" + email)
-        r = requests.put(url = URL)
+        headers = {"pass": "sL36KjRf5oAc79ifhPJAz1bqi03WQPCC"}
+        r = requests.put(url=URL, headers=headers)
+        print("r = ", r)
         # print("statusCode = " + str(r.status_code))
         return r.content.decode()
     except:
         logger.error("\nError occurs when handling api event.\n")
-        return 0
+        return "0"
+
 
 def lambda_handler(event, context):
     logger.info("\nEvent = " + json.dumps(event, indent=2) + "\n")
-    
+
     records = event.get("Records", None)
     method = event.get("httpMethod", None)
-    token = event.get("token", None) # string
-    
+    token = event.get("token", None)  # string
+
     logger.info("\nRecords = " + json.dumps(records, indent=2) + "\n")
     logger.info("\nhttpMethod = " + json.dumps(method, indent=2) + "\n")
     logger.info("\ntoken = " + json.dumps(token, indent=2) + "\n")
-    
+
     if records:
         logger.info("I got an SNS event.")
         handle_sns_event(records)
     elif token:
         logger.info("I got an API Gateway event.")
         res = handle_api_event(token)
-        # Redirect browser:
-        # Successfully activated:
+        logger.info("res = " + res)
+        # REDIRECT
         if res == "1":
-            response={'location': 'https://www.youtube.com/watch?v=WS6jCdLFjrU'}
-        # Account already activated:
+            response = {'location': 'https://www.youtube.com/watch?v=WS6jCdLFjrU'}
         elif res == "None":
-            response={'location': 'https://www.uptrends.com/support/kb/account-access/already-activated'}
-        # Something wen wrong:
+            response = {'location': 'https://www.uptrends.com/support/kb/account-access/already-activated'}
         else:
-            response={'location': 'https://www.youtube.com/watch?v=t3otBjVZzT0'}
+            response = {'location': 'https://www.youtube.com/watch?v=t3otBjVZzT0'}
         return response
 
     else:
